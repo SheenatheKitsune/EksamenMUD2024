e83c51a4e2be6f7edf819cce6744271ee9b456ec
SheenatheKitsune <sheenatheinari@gmail.com>
2024-05-23 13:44:29 +0200

    Small detail changes

--- settings/init.php
+++ settings/init.php
@@ -9,7 +9,7 @@ define("CONFIG_LIVE", "0"); // 0: Test enviroment || 1: Live enviroment
 
 if(CONFIG_LIVE == 0){
     $DB_SERVER = "localhost";
-    $DB_NAME = "";
+    $DB_NAME = "eksamenmud2024";
     $DB_USER = "root";
     $DB_PASS = "";
 }else{
