efaec4562a7a5679a22d10f6c1497cfacc224d98
SheenatheKitsune <sheenatheinari@gmail.com>
2024-05-27 15:05:35 +0200

    Just set the things up, and added javascript, also added some PHP to the code. Just need to changed the colors for the background, and change some small details so it looks better.

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require "settings/init.php";
 <head>
 	<meta charset="utf-8">
 	
-	<title>Sigende titel</title>
+	<title>Home Menu</title>
 	
 	<meta name="robots" content="All">
 	<meta name="author" content="Udgiver">
@@ -18,142 +18,195 @@ require "settings/init.php";
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
+<body>
 <div class="container-fluid">
     <div class="row vh-100">
-        <div class="col-8 vh-100 position-relative bg-success" id="meetingRoom">
+        <div class="vh-100 position-relative bg-success" id="opslagstavle">
             <div class="row row-cols-2 fs-3 text-light border-bottom py-2">
-                <div id="status">
-                    <span>Optaget</span>
-                    <span>Ledigt</span>
+                <div>Hotel Strandparken</div>
+                <div class="text-end" id="time">00:00</div>
+            </div>
+
+            <div class="text-light text-center py-3 mt-3">
+                <div class="fs-1 fw-bold">Velkommen</div>
+                <div class="fs-2">Værelses nummer</div>
+                <div class="fs-3">25</div>
+            </div>
+
+            <div class="d-flex align-items-center justify-content-center" id="logo">
+                <img src="../EksamenMUD2024/Pictures/LogoBlackBackground.png" class="img-fluid w-75" alt="Hotel Strandparken's logo">
+                <img src="../EksamenMUD2024/Pictures/LogoWhiteBackground.png" class="img-fluid w-75" alt="Hotel Strandparken's logo">
+            </div>
+
+            <div class="text-light text-center py-3 fs-1" id="status">
+                <div>Forstyr ikke</div>
+                <div>Forstyr ikke</div>
+            </div>
+
+
+            <!--
+                <div class="w-50 mx-auto text-light py-3" id="cleaningStatus">
+                    <div class="text-center mb-2">Rengøres af:</div>
+                    <div class="card text-light">
+                        <div class="row g-0 justify-content-center">
+                            <div class="col-auto d-flex align-items-center ps-1">
+                                <img src="" class="profile-img rounded-circle border border-2 border-light" alt="profilbillede">
+                            </div>
+                            <div class="col-auto">
+                                <div class="card-body">
+                                    <div class="card-text"><small>Navn</small></div>
+                                    <div class="card-text">Rengørings assitent</div>
+                                    <div class="card-text fw-bold">00:00 - 00:00</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="text-end">12:34</div>
-            </div>
+            -->
 
-            <div class="text-light text-center py-3">
-                <div class="fs-5">#123</div>
-                <div class="fs-1 fw-bold">Lokalenavn</div>
-            </div>
-
-            <div class="text-light text-center py-3">
-                <div>Skærm: Ja</div>
-                <div>Maks. pers: 20</div>
-            </div>
-
-            <div class="w-50 mx-auto text-light py-3" id="occupied">
-                <div class="text-center mb-2">Optaget af:</div>
-                <div class="card text-light">
-                    <div class="row g-0 justify-content-center">
-                        <div class="col-auto d-flex align-items-center ps-1">
-                            <img src="images/uifaces5.jpg" class="profile-img rounded-circle border border-2 border-light" alt="profilbillede">
-                        </div>
-                        <div class="col-auto">
-                            <div class="card-body">
-                                <div class="card-text fw-bold">07:15 - 08:00</div>
-                                <div class="card-text">Afd: Marketingafdeling</div>
-                                <div class="card-text"><small>Adam, Bent, Carl</small></div>
-                            </div>
-                        </div>
-                    </div>
+            <div class="row row-cols-5 position-fixed bottom-0 start-50 translate-middle mb-3">
+                <div class="align-content-center p-3 ps-5">
+                    <button type="button" class="btn btn-light rounded-circle" data-bs-toggle="modal" data-bs-target="#howToVideo"><i class="fa-solid fa-question" style="color: #808080;"></i></button>
                 </div>
-            </div>
-
-            <div class="position-absolute bottom-0 start-50 translate-middle mb-3">
-                <button type="button" id="bookBtn" class="btn btn-light btn-lg px-5 rounded-5" data-bs-toggle="modal" data-bs-target="#bookRoom">Book</button>
-            </div>
-
-        </div>
-
-        <div class="col-4 vh-100 bg-light position-fixed end-0 overflow-y-scroll">
-            <div class="row row-cols-2 fs-3 border-bottom opacity-50 py-2">
-                <div>Næste:</div>
-                <div class="text-end">1. april</div>
-            </div>
-
-            <div class="row">
-                <div id="nextUp">
-                    <div class="card my-3">
-                        <div class="row g-0">
-                            <div class="col-auto d-flex align-items-center ps-1">
-                                <img src="images/uifaces1.jpg" class="profile-img rounded-circle border border-2 border-danger" alt="profilbillede">
-                            </div>
-                            <div class="col">
-                                <div class="card-body">
-                                    <div class="card-text fw-bold">08:00 - 09:30</div>
-                                    <div class="card-text">Afd: Salgsafdeling</div>
-                                    <div class="card-text"><small class="text-body-secondary">Adam, Bent, Carl</small></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="card my-3">
-                        <div class="row g-0">
-                            <div class="col-auto d-flex align-items-center ps-1">
-                                <img src="images/uifaces2.jpg" class="profile-img rounded-circle border border-2 border-danger" alt="profilbillede">
-                            </div>
-                            <div class="col">
-                                <div class="card-body">
-                                    <div class="card-text fw-bold">10:00 - 11:00</div>
-                                    <div class="card-text">Afd: Finansafdeling</div>
-                                    <div class="card-text"><small class="text-body-secondary">Adam, Bent, Carl</small></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="card my-3">
-                        <div class="row g-0">
-                            <div class="col-auto d-flex align-items-center ps-1">
-                                <img src="images/uifaces3.jpg" class="profile-img rounded-circle border border-2 border-danger" alt="profilbillede">
-                            </div>
-                            <div class="col">
-                                <div class="card-body">
-                                    <div class="card-text fw-bold">11:00 - 12:00</div>
-                                    <div class="card-text">Afd: IT-afdeling</div>
-                                    <div class="card-text"><small class="text-body-secondary">Adam, Bent, Carl</small></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="card my-3">
-                        <div class="row g-0">
-                            <div class="col-auto d-flex align-items-center ps-1">
-                                <img src="images/uifaces4.jpg" class="profile-img rounded-circle border border-2 border-danger" alt="profilbillede">
-                            </div>
-                            <div class="col">
-                                <div class="card-body">
-                                    <div class="card-text fw-bold">13:00 - 14:45</div>
-                                    <div class="card-text">Afd: HR og Løn</div>
-                                    <div class="card-text"><small class="text-body-secondary">Adam, Bent, Carl</small></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                <div id="bellRinging">
+                    <button type="button" class="btn btn-light rounded-circle"><img src="../EksamenMUD2024/Pictures/BellIconRinging.png" class="img-fluid p-2" alt="klokke ikon"></button>
+                </div>
+                <div id="bellMuted">
+                    <button type="button" class="btn btn-light rounded-circle"><img src="../EksamenMUD2024/Pictures/BellIconMuted.png" class="img-fluid p-2" alt="klokke ikon muted"></button>
+                </div>
+                <div class="align-content-center">
+                    <button class="btn btn-light btn-lg px-5 rounded-5" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">Roomservice</button>
                 </div>
             </div>
         </div>
     </div>
 </div>
 
-<div class="modal" id="bookRoom" tabindex="-1">
+<div class="modal" id="howToVideo" tabindex="-1">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Modal title</h5>
+                <h5 class="modal-title">Sådan gør du</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <p>Modal body text goes here.</p>
+                <video src="../EksamenMUD2024/Videos/HowToVideo.mp4" class="img-fluid"></video>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
             </div>
         </div>
     </div>
 </div>
 
+<div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+    <div class="offcanvas-header">
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        <div id="timeRoomservice">00:00</div>
+    </div>
+    <div class="offcanvas-body">
+            <div class="row">
+                <?php
+                $pictures = $db->sql("SELECT * FROM pictures ORDER BY ProdID asc");
+                foreach($pictures as $pictures){
+                ?>
+                <div>
+                    <div class="card my-3">
+                        <div class="row g-0">
+                            <div class="col-4 d-flex p-2">
+                                <img src="../EksamenMUD2024/Pictures/<?php echo $pictures->ProdPic;?>" class="img-fluid rounded-circle" alt="profilbillede">
+                            </div>
+                            <div class="col-auto">
+                                <div class="card-body">
+                                    <div class="card-text"><small class="text-body-secondary">Navn</small></div>
+                                    <div class="card-text">Rengørings assistent</div>
+                                    <div class="card-text fw-bold">00:00 - 00:00</div>
+                                </div>
+                            </div>
+                        </div>
+                        <?php
+                        }
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="https://kit.fontawesome.com/96a3a7d865.js" crossorigin="anonymous"></script>
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script>
+    const opslagstavle = document.querySelector('#opslagstavle');
+    const logo = document.querySelector('#logo');
+    const status = document.querySelector('#status');
+    const bellRinging = document.querySelector('#bellRinging');
+    const bellMuted = document.querySelector('#bellMuted');
+    const time = document.querySelector('#time');
+    const timeRoomserive = document.querySelector('#timeRoomservice');
+
+    bellRinging.addEventListener('click', () => {
+        toggleStatus(false);
+    })
+
+    bellMuted.addEventListener('click', () => {
+        toggleStatus(true);
+    })
+
+    function toggleStatus(isBellMuted){
+
+        status.children[0].classList.remove('d-none');
+        status.children[1].classList.remove('d-none');
+        logo.children[0].classList.remove('d-none');
+        logo.children[1].classList.remove('d-none');
+
+        if(isBellMuted){
+            opslagstavle.classList.remove('bg-succes');
+            opslagstavle.classList.add('bg-danger');
+
+            status.children[0].classList.add('d-none');
+            status.children[1].classList.add('d-block');
+
+            logo.children[0].classList.add('d-none');
+            logo.children[1].classList.add('d-block');
+
+        } else {
+            opslagstavle.classList.add('bg-success');
+            opslagstavle.classList.remove('bg-danger');
+
+            status.children[0].classList.add('d-none');
+            status.children[1].classList.add('d-none');
+
+            logo.children[0].classList.add('d-block');
+            logo.children[1].classList.add('d-none');
+        }
+
+    }
+
+    function showTime(){
+        const date = new Date();
+        const hours = date.getHours().toString().padStart(2, 0);
+        const minutes = date.getMinutes().toString().padStart(2, 0);
+        const clock = hours + ':' + minutes;
+        time.innerHTML = clock;
+
+        setTimeout(showTime, 1000);
+    }
+
+    showTime();
+
+    function showTimeRoomservice(){
+        const date = new Date();
+        const hours = date.getHours().toString().padStart(2, 0);
+        const minutes = date.getMinutes().toString().padStart(2, 0);
+        const clock = hours + ':' + minutes;
+        timeRoomserive.innerHTML = clock;
+
+        setTimeout(showTimeRoomservice, 1000);
+    }
+
+    showTimeRoomservice();
+
+</script>
 </body>
 </html>
